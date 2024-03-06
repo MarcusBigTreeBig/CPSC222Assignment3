0885c28a03127d52de115209cdbe8b1077d81198
MarcusBigTreeBig <the.marcus.herbert@gmail.com>
2024-03-05 20:11:37 -0800

    randomized order of creation of nodes

--- src/marcus_herbert/LCRMain.java
+++ src/marcus_herbert/LCRMain.java
@@ -1,6 +1,7 @@
 package marcus_herbert;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Based on code written by Colton Aarts
@@ -14,15 +15,23 @@ public class LCRMain {
     public static void main(String[] args) {
         ArrayList<LCRNode> nodes = new ArrayList<>();
         LCRNodeStart nodeStart = new LCRNodeStart(0);
-        for(int i = 0; i < 10; i++){
+        int n = 10;
+        for(int i = 0; i < n; i++){
             LCRNode node = new LCRNode(i+1);
             nodes.add(node);
         }
 
-        for(int i = 1; i < 9; i++){
-            LCRNode node = nodes.get(i);
-            node.setLeft(nodes.get(i-1));
-            node.setRight(nodes.get(i+1));
+        //use a randomly arranged ArrayList to initialize nodes in random order
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        Collections.shuffle(list);
+
+        for(int i = 1; i < n-1; i++){
+            LCRNode node = nodes.get(list.get(i));
+            node.setLeft(nodes.get(list.get(i-1)));
+            node.setRight(nodes.get(list.get(i+1)));
         }
         nodes.get(nodes.size()-1).setRight(nodeStart);
         nodes.get(nodes.size()-1).setLeft(nodes.get(nodes.size()-2));
